6b7eca80dca8fc469bfa9c9d66e61909de4206c7
Juan Felipe <80426592+GomezJuanEfe@users.noreply.github.com>
2022-06-05 16:31:55 -0700

    Update! Ready to work as a team!

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>ñ
+  <>
   <h1>Hello World! This is the VanCoders Project!</h1>
   {/* Put your components here (import) and put the code on the components folder. */}
   </>
