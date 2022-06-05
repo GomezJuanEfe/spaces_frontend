b63ffc3ca80b216ae1677094d020a41e13ec43b3
tamwoodsira <siramonc@gmail.com>
2022-06-05 16:53:59 -0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+function Header() {
+    return(
+        <>
+            <img src="https://www.coworker.com/mag/wp-content/uploads/2017/10/NEST-@-TRYP-hotel-1280x640.jpg" alt="Header" class="header" />
+        </>
+    )
+}
+export default Header;
