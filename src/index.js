49886868c4772589328570c3dc1daaa12a7ffb7d
MINJI <alswl162@gmail.com>
2022-06-06 09:06:50 +0900

    our first clients commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import OurClients from './components/OurClients';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-  <h1>Hello World! This is the VanCoders Project!</h1>
-  {/* Put your components here (import) and put the code on the components folder. */}
+  <OurClients/>
   </>
 );
