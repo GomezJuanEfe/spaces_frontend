749cd6637435b65eafad67612d9b83d400e12595
Juan Gomez <jf.gomezramirez@gmail.com>
2022-06-07 01:41:49 -0700

    Routin and login page done

---
+++ src/components/Navigation.js
@@ -0,0 +1,17 @@
+function Navigation() {
+  return (
+    <>
+      <section className="navBar">
+        <div className="navBar-bottom">
+          <ul>
+            <li><a href='#'>Home</a></li>
+            <li><a href='#'>About Us</a></li>
+            <li><a href='#'>Our Clients</a></li>
+          </ul>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Navigation;
