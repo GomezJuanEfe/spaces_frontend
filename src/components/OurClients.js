7d9507258507b3b687340f64ca3844be4ed73259
MINJI <alswl162@gmail.com>
2022-06-09 07:36:45 +0900

    Clients review done

--- src/components/OurClients.js
+++ src/components/OurClients.js
@@ -1,28 +1,66 @@
 function OurClients () {
     return(
         <>
+        <section class= "clients_index">
+        <h2>Our Clients</h2>
+        
+        <p>Trusted and Loved by</p>
+        </section>
+        
         <section class="clients_logo">
+       
          <div>
-             <img src="https://kwanjaiservices.com/wp-content/uploads/2020/01/sarin-city.png" alt="client1"/>
+             <img src="https://kwanjaiservices.com/wp-content/uploads/2020/01/sarin-city.png" alt="client1" class="clients"/>
          </div>
          
          <div>
-            <img src="https://makeupbydesign.ca/wp-content/uploads/2020/07/marika-magazine-logo-275x275-1.png" alt="client2"/>
+            <img src="https://makeupbydesign.ca/wp-content/uploads/2020/07/marika-magazine-logo-275x275-1.png" alt="client2" class="clients"/>
          </div>
 
          <div>
-           <img src="https://d1fdloi71mui9q.cloudfront.net/SVDJcKMLRWSCGSKTjwVN_1976cda9e0a0a931b2ca2148ef63f2e8.png" alt="client3"/>
+           <img src="https://d1fdloi71mui9q.cloudfront.net/SVDJcKMLRWSCGSKTjwVN_1976cda9e0a0a931b2ca2148ef63f2e8.png" alt="client3" class="clients"/>
          </div>
 
          <div>
-           <img src="https://www.careersinafrica.com/wp-content/uploads/2013/07/orange-logo-vector.png" alt="client4"/>  
+           <img src="https://www.careersinafrica.com/wp-content/uploads/2013/07/orange-logo-vector.png" alt="client4" class="clients"/>  
          </div>
      
         </section>
+        
+
+        <section class="review-section">
+            <div>
+                <h2>What our customers say about us</h2>
+            </div>
+
+            <article class="profile">
+                
+                <div class="profile1">
+                    <img src="https://images.iphonephotographyschool.com/24755/portrait-photography.jpg" alt="review1" class="reviews"/>
+                    <div>
+                        <p>“Having my Spaces On Demand app makes it very flexible to meet with people I need to see in different cities.”</p>
+                        <p>Hamel</p>
+                    </div>
+                </div>
+
+                <div class="profile2">
+                    <img src="https://nadiazheng.com/wp-content/uploads/2020/06/Montreal-actress-headshot-studio-portrait-black-turtleneck-black-hair-by-nadia-zheng-800x1000.jpg" alt="review2" class="reviews"/>
+            
+                    <div>
+                    <p>"The offices look professional and feel professional. It’s a great alternative to having my own office.” </p>
+                    <p>Jenny</p>
+                    </div>
+                </div>
+            </article>
+
+        </section> 
+
         </>
-    )
+    
+
+    
       
     
-}
+)}
 
 export default OurClients;
