018835b81ef4801dc70c3b8f21fb381ca3fd6cd7
tamwoodsira <siramonc@gmail.com>
2022-06-09 00:56:57 -0700

    styled contact icon

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -16,31 +16,15 @@ function Contact(){
         <>
         <section className={'matSection'}>
           <div className={'text'}>
-            <h1>Contact us</h1>
+            <h2>Contact us</h2>
             <p>
               Register. Stay connected. <br/> Get involved with us on our social media platforms.
             </p>
-          </div>
-          <div className={'text'} id="contact">
-              <p>
-                Register and book our Spaces today.
-              </p>
-          </div>
-          <div className="text2">
-            <p>
-                Get involved on our social media platforms.
-            </p>
-          <ul>
-            <li>
-              <a href="https://www.facebook.com" target={'_blank'}>Facebook</a>
-            </li>
-            <li>
-              <a href="https://www.instagram.com" target={'_blank'}>Instagram</a>
-            </li>
-            <li>
-              <a href="https://www.twitter.com" target={'_blank'}>Twitter</a>
-            </li>
-          </ul>
+                <a href="https://www.facebook.com" target={'_blank'}><img src="https://i.pinimg.com/originals/c1/c8/53/c1c853671c00982573a1b45e777d7725.png" alt="Instagram Icon" class="icon"/></a>
+              
+                <a href="https://www.instagram.com" target={'_blank'}><img src="https://cdn.icon-icons.com/icons2/2428/PNG/512/instagram_black_logo_icon_147122.png" alt="Instagram Icon" class="icon"/></a>
+              
+                <a href="https://www.twitter.com" target={'_blank'}><img src="https://images.squarespace-cdn.com/content/v1/5d6523921b6c650001c8ea9b/1567029621475-3C89F7DSH2RB2YGCZN5Q/23681.png" alt="Twitter Icon" class="icon"/></a>
           </div>
           <div className={'contact_form'}>
             <form method="POST" action="http://localhost/FinalProject_phpCourse/pages/manager/registerUserFront.php" onSubmit={(event) => handleSubmit(event)} >
